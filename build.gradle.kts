8f044d89454ecf2d2c02ad986532382b90667f67
manami-project <13179375+manami-project@users.noreply.github.com>
2020-08-28 16:46:39 +0200

    Updated modb-core-to 2.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ version = project.findProperty("releaseVersion") as String? ?: ""
 val projectName = "modb-db-parser"
 
 dependencies {
-    api("io.github.manamiproject:modb-core:2.1.1")
+    api("io.github.manamiproject:modb-core:2.1.2")
 
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
