9d0c6cea2b4d0aef2f1539f7475dc7aace443ec1
manami-project <13179375+manami-project@users.noreply.github.com>
2020-09-17 19:13:16 +0200

    Added logback for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 
     implementation(platform(kotlin("bom")))
 
+    testImplementation("ch.qos.logback:logback-classic:1.2.3")
     testImplementation("io.github.manamiproject:modb-test:1.0.4")
 }
 
