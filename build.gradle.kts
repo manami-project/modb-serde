1413c203aa0ffe5db37c729a5f9b5c34414f46a1
manami-project <13179375+manami-project@users.noreply.github.com>
2021-03-30 12:46:22 +0200

    Updated modb-core to 3.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ version = project.findProperty("releaseVersion") as String? ?: ""
 val projectName = "modb-db-parser"
 
 dependencies {
-    api("io.github.manamiproject:modb-core:3.1.2")
+    api("io.github.manamiproject:modb-core:3.2.3")
 
     implementation(platform(kotlin("bom")))
 
