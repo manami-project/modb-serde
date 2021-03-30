eff831c70e7820c467e82ff248ae7e9d166c20db
manami-project <13179375+manami-project@users.noreply.github.com>
2021-03-30 12:46:36 +0200

    Updated modb-test to 1.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation(platform(kotlin("bom")))
 
     testImplementation("ch.qos.logback:logback-classic:1.2.3")
-    testImplementation("io.github.manamiproject:modb-test:1.2.2")
+    testImplementation("io.github.manamiproject:modb-test:1.2.3")
 }
 
 kotlin {
