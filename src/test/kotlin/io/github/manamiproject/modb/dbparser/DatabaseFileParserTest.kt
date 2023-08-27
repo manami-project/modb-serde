a7fe77d51c911839442aeb85b35ff2e44ec604aa
manami-project <13179375+manami-project@users.noreply.github.com>
2023-08-27 18:40:15 +0200

    Adjustments for modb-core 9.0.0

--- src/test/kotlin/io/github/manamiproject/modb/dbparser/DatabaseFileParserTest.kt
+++ src/test/kotlin/io/github/manamiproject/modb/dbparser/DatabaseFileParserTest.kt
@@ -23,7 +23,7 @@ internal class DatabaseFileParserTest : MockServerTestCase<WireMockServer> by Wi
         fun `throws exception if the response code is not 200`() {
             // given
             val testHttpClient = object: HttpClient by TestHttpClient {
-                override suspend fun get(url: URL, headers: Map<String, Collection<String>>, retryWith: String): HttpResponse = HttpResponse(500, "ERROR")
+                override suspend fun get(url: URL, headers: Map<String, Collection<String>>): HttpResponse = HttpResponse(500, "ERROR")
             }
 
             val defaultDatabaseFileParser = DatabaseFileParser(
@@ -44,7 +44,7 @@ internal class DatabaseFileParserTest : MockServerTestCase<WireMockServer> by Wi
         fun `throws exception if the response body is blank`() {
             // given
             val testHttpClient = object: HttpClient by TestHttpClient {
-                override suspend fun get(url: URL, headers: Map<String, Collection<String>>, retryWith: String): HttpResponse = HttpResponse(200, EMPTY)
+                override suspend fun get(url: URL, headers: Map<String, Collection<String>>): HttpResponse = HttpResponse(200, EMPTY)
             }
 
             val defaultDatabaseFileParser = DatabaseFileParser(
@@ -66,7 +66,7 @@ internal class DatabaseFileParserTest : MockServerTestCase<WireMockServer> by Wi
             runBlocking {
                 // given
                 val testHttpClient = object: HttpClient by TestHttpClient {
-                    override suspend fun get(url: URL, headers: Map<String, Collection<String>>, retryWith: String): HttpResponse = HttpResponse(
+                    override suspend fun get(url: URL, headers: Map<String, Collection<String>>): HttpResponse = HttpResponse(
                         code = 200,
                         body = loadTestResource("test_db_for_deserialization.json"),
                     )
